b0a4acffec78c7816d995fe9a3c2c8005a297ae0
John Davis <john@chess.bot>
2020-08-30 18:12:31 -0500

    Updated report ARNs.

--- packages/infrastructure/lib/cicd/build-project.ts
+++ packages/infrastructure/lib/cicd/build-project.ts
@@ -1,16 +1,22 @@
 import * as cdk from '@aws-cdk/core';
+import * as iam from '@aws-cdk/aws-iam';
 import * as kms from '@aws-cdk/aws-kms';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as codebuild from '@aws-cdk/aws-codebuild';
 import { HerculesAccount } from '../accounts';
 
+interface PackageReportGroups {
+  tests: codebuild.IReportGroup,
+  coverage: codebuild.IReportGroup,
+}
+
 interface ReportGroups {
-  website: codebuild.IReportGroup,
-  typeScriptClient: codebuild.IReportGroup,
-  api: codebuild.IReportGroup,
-  triggers: codebuild.IReportGroup,
-  model: codebuild.IReportGroup,
-  dotnetClient: codebuild.IReportGroup
+  website: PackageReportGroups,
+  typeScriptClient: PackageReportGroups,
+  api: PackageReportGroups,
+  triggers: PackageReportGroups,
+  model: PackageReportGroups,
+  dotnetClient: PackageReportGroups
 }
 
 interface BuildProjectProps {
@@ -32,58 +38,111 @@ export class BuildProject extends cdk.Construct {
       encryptionKey: reportEncryptionKey
     });
 
-    const websiteReportGroup = new codebuild.ReportGroup(this, 'WebsiteReportGroup', {
-      reportGroupName: 'hercules-website',
-      exportBucket: buildReportsBucket,
-      zipExport: true
-    });
-    const typescriptClientReportGroup = new codebuild.ReportGroup(this, 'TypeScriptClientReportGroup', {
-      reportGroupName: 'hercules-typescript-client',
-      exportBucket: buildReportsBucket,
-      zipExport: true
-    });
-    const apiReportGroup = new codebuild.ReportGroup(this, 'ApiReportGroup', {
-      reportGroupName: 'Hercules.Api',
-      exportBucket: buildReportsBucket,
-      zipExport: true
-    });
-    const triggersReportGroup = new codebuild.ReportGroup(this, 'TriggersReportGroup', {
-      reportGroupName: 'Hercules.Triggers',
-      exportBucket: buildReportsBucket,
-      zipExport: true
-    });
-    const modelReportGroup = new codebuild.ReportGroup(this, 'ModelReportGroup', {
-      reportGroupName: 'Hercules.Api.Model',
-      exportBucket: buildReportsBucket,
-      zipExport: true
-    });
-    const clientReportGroup = new codebuild.ReportGroup(this, 'ClientReportGroup', {
-      reportGroupName: 'Hercules.Api.Client',
-      exportBucket: buildReportsBucket,
-      zipExport: true
-    });
+    const createDotNetDualReportGroups = (scope: cdk.Construct, bucket: s3.IBucket, name: string) : PackageReportGroups => {
 
+      const testsReportGroup = new codebuild.ReportGroup(scope, `${name}TestsReportGroup`, {
+        reportGroupName: `${name}-tests`,
+        exportBucket: bucket,
+        zipExport: true
+      });
+      (testsReportGroup.node.defaultChild as codebuild.CfnReportGroup).addPropertyOverride("Name", `${name}-tests`);
+      (testsReportGroup.node.defaultChild as codebuild.CfnReportGroup).addPropertyOverride("Type", 'TEST');
+    
+      const coverageReportGroup = new codebuild.ReportGroup(scope, `${name}CoverageReportGroup`, {
+        reportGroupName: `${name}-coverage`,
+        exportBucket: bucket,
+        zipExport: true
+      });
+      (coverageReportGroup.node.defaultChild as codebuild.CfnReportGroup).addPropertyOverride("Name", `${name}-coverage`);
+      (coverageReportGroup.node.defaultChild as codebuild.CfnReportGroup).addPropertyOverride("Type", 'CODE_COVERAGE');
+    
+      return {
+        tests: testsReportGroup,
+        coverage: coverageReportGroup
+      }
+    };
+    
     this.reportGroups = {
-      api: apiReportGroup,
-      triggers: triggersReportGroup,
-      website: websiteReportGroup,
-      typeScriptClient: typescriptClientReportGroup,
-      dotnetClient: clientReportGroup,
-      model: modelReportGroup
+      api: createDotNetDualReportGroups               (this, buildReportsBucket, 'hercules-api'),
+      triggers: createDotNetDualReportGroups          (this, buildReportsBucket, 'hercules-triggers'),
+      dotnetClient: createDotNetDualReportGroups      (this, buildReportsBucket, 'hercules-dotnet-client'),
+      model: createDotNetDualReportGroups             (this, buildReportsBucket, 'hercules-api-model'),
+      website: createDotNetDualReportGroups           (this, buildReportsBucket, 'hercules-website'),
+      typeScriptClient: createDotNetDualReportGroups  (this, buildReportsBucket, 'hercules-typescript-client'),
     }
 
+    const buildRole = new iam.Role(this, 'BuildRole', {
+      assumedBy: new iam.CompositePrincipal(
+        new iam.ServicePrincipal('codebuild.amazonaws.com'),
+        new iam.ServicePrincipal('codepipeline.amazonaws.com')
+      )
+    });
 
     const buildProject = new codebuild.PipelineProject(this, 'Project', {
       environment: {
         computeType: codebuild.ComputeType.LARGE,
         buildImage: codebuild.LinuxBuildImage.STANDARD_4_0,
         privileged: true
-      }
+      },
+      role: buildRole
     });
-    Object.values(this.reportGroups).forEach(v => {
-      const rg = v as codebuild.IReportGroup;
-      rg.grantWrite(buildProject.role!);
-    }); 
+
+    const reportingPolicy = new iam.PolicyStatement();
+    reportingPolicy.addActions(
+      "codebuild:BatchGetBuildBatches",
+      "codebuild:ListReportsForReportGroup",
+      "codebuild:DescribeTestCases",
+      "codebuild:GetResourcePolicy",
+      "codebuild:ListReportGroups",
+      "codebuild:DescribeCodeCoverages",
+      "codebuild:ListBuildsForProject",
+      "codebuild:BatchGetBuilds",
+      "codebuild:CreateReport",
+      "codebuild:UpdateReport",
+      "codebuild:ListReports",
+      "codebuild:BatchPutCodeCoverages",
+      "codebuild:ListProjects",
+      "codebuild:BatchGetReportGroups",
+      "codebuild:ListConnectedOAuthAccounts",
+      "codebuild:BatchGetProjects",
+      "codebuild:BatchGetReports",
+      "codebuild:ListCuratedEnvironmentImages",
+      "codebuild:ListSourceCredentials",
+      "codebuild:ListRepositories",
+      "codebuild:ListSharedProjects",
+      "codebuild:ListBuildBatches",
+      "codebuild:ListSharedReportGroups",
+      "codebuild:ListBuilds",
+      "codebuild:ListBuildBatchesForProject",
+      "codebuild:BatchPutTestCases"
+    );
+    reportingPolicy.addAllResources();
+
+    const bucketPolicy = new iam.PolicyStatement();
+    bucketPolicy.addActions("s3:*");
+    bucketPolicy.addResources(
+      buildReportsBucket.bucketArn,
+      `${buildReportsBucket.bucketArn}/*`);
+
+    const kmsPolicy = new iam.PolicyStatement();
+    kmsPolicy.addActions(
+      "kms:Decrypt",
+      "kms:DescribeKey",
+      "kms:Encrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*"
+    );
+    kmsPolicy.addResources(reportEncryptionKey.keyArn);
+    
+    const codeBuildPolicy = new iam.PolicyStatement();
+    codeBuildPolicy.addActions('logs:*');
+    codeBuildPolicy.addActions('cloudwatch:*');
+    codeBuildPolicy.addAllResources();
+
+    buildRole.addToPolicy(reportingPolicy);
+    buildRole.addToPolicy(bucketPolicy);
+    buildRole.addToPolicy(kmsPolicy);
+    buildRole.addToPolicy(codeBuildPolicy);
 
     this.project = buildProject;
   }
