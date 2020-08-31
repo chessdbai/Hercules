b991d31769d8ad7985ae6370e64cf0a5ead77981
John Davis <john@chess.bot>
2020-08-31 15:53:45 -0500

    Updated website

--- packages/infrastructure/lib/website/website-stack.ts
+++ packages/infrastructure/lib/website/website-stack.ts
@@ -12,10 +12,6 @@ export class WebsiteStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props: WebsiteStackProps) {
     super(scope, id, props);
 
-    const publicZone = r53.PublicHostedZone.fromLookup(this, 'PublicZone', {
-      domainName: props.domainName
-    });
-
     const publicZone = r53.PublicHostedZone.fromHostedZoneId(this, 'PublicZone', props.publicZoneId);
 
     const websiteCertArn = cdk.Fn.importValue('WebsiteCertArn');
