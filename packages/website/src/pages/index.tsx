7816282b90ac0d27b95716d63bc5ae4fcf45f499
John Davis <john@chess.bot>
2020-09-02 23:51:19 -0500

    Updated

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import AccountPage from './AccountPage';
+import LoginPage from './user/LoginPage';
 
 export {
   HomePage,
   AboutPage,
-  AccountPage
+  AccountPage,
+  LoginPage
 };
