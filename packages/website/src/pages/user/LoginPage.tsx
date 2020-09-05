4f34084d4daeb01e58833d5654ce021c52dd70ee
John Davis <john@chess.bot>
2020-09-05 12:31:44 -0500

    Updated

--- packages/website/src/pages/user/LoginPage.tsx
+++ packages/website/src/pages/user/LoginPage.tsx
@@ -1,8 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import { Auth } from 'aws-amplify';
 import { CognitoUser } from 'amazon-cognito-identity-js';
-import { Form, Input, Button, Checkbox, Spin, Space } from 'antd';
+import {
+  Form, Input,
+  Button, Checkbox,
+  Spin, Space
+} from 'antd';
+import { LockOutlined } from '@ant-design/icons';
 import { useHistory, Link } from "react-router-dom";
+
+type MfaType = 'SMS_MFA' | 'SOFTWARE_TOKEN_MFA' | null;
 
 export const formItemLayout = {
   labelCol: {
@@ -30,9 +37,16 @@ interface FormValues {
 
 export default function LoginPage() {
 
+  let [mfaCode, setMfaCode] = useState('');
+  let [password, setPassword] = useState('');
+  let [mfaType, setMfaType] = useState(null as MfaType);
+  let [username, setUsername] = useState('');
+  let [errorMessage, setErrorMessage] = useState('');
   let [loading, setLoading] = useState(false);
 
   let history = useHistory();
+
+
 
   const onFinish = async (values : FormValues) => {
     console.log('Form values:');
@@ -40,17 +54,70 @@ export default function LoginPage() {
     const username = values.username;
     const password = values.password;
     try {
-      var response = await Auth.signIn(username, password)!;
+      var user = await Auth.signIn(username, password)!;
+
+      console.log(user);
+      if (user.challengeName === 'SMS_MFA' ||
+            user.challengeName === 'SOFTWARE_TOKEN_MFA') {
+            
+        } else if (user.challengeName === 'NEW_PASSWORD_REQUIRED') {
+          const {requiredAttributes} = user.challengeParam;
+          
+        }
       history.push('/');
     } catch (err) {
-      const error = err as Error;
-      console.error(error);
+      console.log(err);
+      if (err.name == 'PasswordResetRequiredException') {
+        history.push('/security/update-password');
+      } else if (err.Name == "UserNotFoundException") {
+        setErrorMessage('No user with that name was found.');
+      } else if (err.Name == "UserNotConfirmedException") {
+        setErrorMessage('The user exists but requires MFA confirmation before becoming active.');
+      }
     }
   };
 
   const onFinishFailed = (errorInfo: any) => {
     console.log('Failed:', errorInfo);
   };
+
+  const handleMfaSubmit = async (e: any) => {
+    await Auth.confirmSignIn(
+      username,
+      mfaCode,
+      mfaType
+    );
+    history.push('/home');
+  };
+
+  const renderMfaForm = () => {
+    var loader = !loading ? <></> : <Spin tip="Loading..." />
+    return (
+      <Form className="mfa-form">
+        <Form.Item>
+            <Input
+              prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="MFA Code"
+              onChange={(e: ChangeEvent<HTMLInputElement>) => setMfaCode(e.target.value)}
+            />
+        </Form.Item>
+        {errorMessage !== undefined && errorMessage !== null && errorMessage !== '' ?
+          <span style={{color: 'red'}}></span>
+        : <></>}
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+            disabled={username === undefined || password === undefined}
+            onClick={(e) => handleMfaSubmit(e)}>
+            Confirm MFA Code
+          </Button>
+          {loader}
+        </Form.Item>
+      </Form>
+    )
+  }  
 
   return (
     <Form
@@ -65,7 +132,9 @@ export default function LoginPage() {
         name="username"
         rules={[{ required: true, message: 'Please input your username!' }]}
       >
-        <Input />
+        <Input onChange={(e) => {
+          setUsername(e.target.value);
+        }} />
       </Form.Item>
 
       <Form.Item
